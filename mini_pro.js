65d2faf4060a64d66a4145fea2e539838705e730
alpaca0813 <44792866+alpaca0813@users.noreply.github.com>
2019-01-14 11:12:20 +0900

    Add files via upload

---
+++ mini_pro.js
@@ -0,0 +1,14 @@
+function golevel1(){
+  if (confirm("レベル１を本当にしますか？")==true)
+    location.href = "file:///Users/yhashimoto/minipro.github.io/mini_pro_page2.html";
+}
+
+function golevel2(){
+  if (confirm("レベル２を本当にしますか")==true)
+    location.href = "file:///Users/yhashimoto/minipro.github.io/mini_pro_page3.html";
+}
+
+function golevel3(){
+  if (confirm("レベル３を本当にしますか")==true)
+    location.href = "file:///Users/yhashimoto/minipro.github.io/mini_pro_page4.html";
+}
